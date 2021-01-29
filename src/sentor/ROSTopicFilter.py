60afc95973d5beb834dc54bfb100942a1d0b0754
adambinch <abinch@sagarobotics.com>
2021-01-29 14:35:56 +0000

    Can process every nth message

--- src/sentor/ROSTopicFilter.py
+++ src/sentor/ROSTopicFilter.py
@@ -10,10 +10,12 @@ import rospy, math, numpy
 
 class ROSTopicFilter(object):
 
-    def __init__(self, topic_name, lambda_fn_str, config):
+    def __init__(self, topic_name, lambda_fn_str, config, throttle_val):
         self.topic_name = topic_name
         self.lambda_fn_str = lambda_fn_str
         self.config = config
+        self.throttle_val = throttle_val
+        self.throttle = self.throttle_val
         
         self.lambda_fn = None
         try:
@@ -30,8 +32,10 @@ class ROSTopicFilter(object):
         self.value_read = False
         self.sat_callbacks = []
         self.unsat_callbacks = []
+        
 
     def callback_filter(self, msg):
+        
         if self.lambda_fn is None:
             return
 
@@ -54,8 +58,16 @@ class ROSTopicFilter(object):
         else:
             for func in self.unsat_callbacks:
                 func(self.lambda_fn_str)
+                
 
-
+    def callback_filter_throttled(self, msg):
+        
+        if (self.throttle % self.throttle_val) == 0:
+            self.callback_filter(msg)
+            self.throttle = 1
+        else:
+            self.throttle += 1
+            
         # if not self.filter_satisfied and not self.value_read:
         #     self.filter_satisfied = self.lambda_fn(value)
 
@@ -70,10 +82,12 @@ class ROSTopicFilter(object):
             return True
 
         return self.filter_satisfied
+    
 
     def register_satisfied_cb(self, func):
 
         self.sat_callbacks.append(func)
+        
 
     def register_unsatisfied_cb(self, func):
 
