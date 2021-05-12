53238f1e9da237e70fd64474b365e6237d82379e
adambinch <abinch@sagarobotics.com>
2021-05-12 16:36:32 +0100

    Sentor/monitors topic reports all conditions (not just safety critical ones)
    New field added to msg definition indicating whether the condition is safety critical (or not)

--- src/sentor/MultiMonitor.py
+++ src/sentor/MultiMonitor.py
@@ -32,7 +32,7 @@ class MultiMonitor(object):
         
         if not self._stop_event.isSet():
             
-            error_code_new = [monitor.crit_conditions[expr]["safe"] for monitor in self.topic_monitors for expr in monitor.crit_conditions]
+            error_code_new = [monitor.conditions[expr]["satisfied"] for monitor in self.topic_monitors for expr in monitor.conditions]
             
             if error_code_new != self.error_code:
                 self.error_code = error_code_new
@@ -44,13 +44,14 @@ class MultiMonitor(object):
                 for monitor in self.topic_monitors:
                     topic_name = monitor.topic_name
                     
-                    for expr in monitor.crit_conditions:
+                    for expr in monitor.conditions:
                         condition = Monitor()
                         condition.topic = topic_name
-                        condition.expression = expr
-                        condition.safe = self.error_code[count]
-                        condition.tags = monitor.crit_conditions[expr]["tags"]
-                        conditions.monitors.append(condition)
+                        condition.condition = expr
+                        condition.safety_critical = monitor.conditions[expr]["safety_critical"]
+                        condition.satisfied = self.error_code[count]
+                        condition.tags = monitor.conditions[expr]["tags"]
+                        conditions.conditions.append(condition)
                         count+=1
                         
                 self.monitors_pub.publish(conditions)
